6fbaf45404addd161176cea7fa7cbe08484dc49a
AimenAlamin <alaminaimen@gmail.com>
2024-11-29 14:28:03 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:al_pazar/firebase_options.dart';
 import 'package:al_pazar/pazar_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/routing/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // To fix texts being hidden bug in flutter_screenutil in release mode.
 
   runApp(PazarApp(
